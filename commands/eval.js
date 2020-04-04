6e87f506016615ed6cb2651e588f6443cfc0be11
Eflatun <aliismailozcann@gmail.com>
2020-04-04 12:20:49 +0300

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -1,1 +1,20 @@
-exports.run = async (Client, message, args) => { 	 	try { 		const code = args.join(" "); 		let evaledCode = eval(code); 			 		if (typeof evaledCode !== "string") 		evaledCode = require("util").inspect(evaledCode); 		message.channel.send("Output:"+evaledCode); 	} catch (err) { message.channel.send(err); } 	 }  exports.config = { 	 	name: "eval", 	usage: "[prefix]eval [arguments]", 	isOwnerOnly: true 	 }
+exports.run = async (Client, message, args) => {
+	
+	try {
+		const code = args.join(" ");
+		let evaledCode = eval(code);
+			
+		if (typeof evaledCode !== "string")
+		evaledCode = require("util").inspect(evaledCode);
+		message.channel.send("Output:"+evaledCode);
+	} catch (err) { message.channel.send(err); }
+	
+}
+
+exports.config = {
+	
+	name: "eval",
+	usage: "[prefix]eval [arguments]",
+	isOwnerOnly: true
+	
+}
