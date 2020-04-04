a8806111ef6cc27eea06d44f632546c642d709ac
Eflatun <aliismailozcann@gmail.com>
2020-04-04 12:54:33 +0300

    Updated ".log" to ".warn"

--- functions.js
+++ functions.js
@@ -6,7 +6,7 @@ module.exports = (Client) => {
 	fs.readdir(`./${storage.files.commandFolder}/`, (err, files) => {
 		if(err) return console.log("[Error] "+err);
 		let commands = files.filter(file => file.split(".").pop() === "js");
-		if(commands.length <= 0) return console.log("[Warning] No command files were found.");
+		if(commands.length <= 0) return console.warn("[Warning] No command files were found.");
 	
 		commands.forEach((f, i) => {
 			const command = require(`./${storage.files.commandFolder}/${f}`);
@@ -17,7 +17,7 @@ module.exports = (Client) => {
 	fs.readdir(`./${storage.files.eventFolder}/`, (err, files) => {
 		if(err) return console.log("[Error] "+err);
 		let jsfiles = files.filter(f => f.split(".").pop() === "js");
-		if (jsfiles.length <= 0) return console.log("[Warning] No event files were found.");
+		if (jsfiles.length <= 0) return console.warn("[Warning] No event files were found.");
 	
 		jsfiles.forEach((f, i) => {
 			require(`./events/${f}`);
